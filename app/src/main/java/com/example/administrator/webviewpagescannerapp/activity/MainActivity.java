8b51aad3e08ae62eb161edbf0338c983be36be2d
xiaoxiao <876610383@qq.com>
2017-03-16 18:31:30 +0800

    1.修复了无图页面导致程序报错阻塞其他操作的问题；

--- app/src/main/java/com/example/administrator/webviewpagescannerapp/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/webviewpagescannerapp/activity/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         contentWebView = (WebView) findViewById(R.id.webView);
         contentWebView.getSettings().setJavaScriptEnabled(true);
+        contentWebView.getSettings().setAppCacheEnabled(true);
+        contentWebView.getSettings().setDatabaseEnabled(true);
+        contentWebView.getSettings().setDomStorageEnabled(true);
         contentWebView.loadUrl("http://a.mp.uc.cn/article.html?uc_param_str=frdnsnpfvecpntnwprdssskt&client=ucweb&wm_aid=c51bcf6c1553481885da371a16e33dbe&wm_id=482efebe15ed4922a1f24dc42ab654e6&pagetype=share&btifl=100");
         contentWebView.addJavascriptInterface(new MJavascriptInterface(this,imageUrls), "imagelistener");
         contentWebView.setWebViewClient(new MyWebViewClient());
--- app/src/main/java/com/example/administrator/webviewpagescannerapp/tool/StringUtils.java
+++ app/src/main/java/com/example/administrator/webviewpagescannerapp/tool/StringUtils.java
@@ -1,5 +1,7 @@
 package com.example.administrator.webviewpagescannerapp.tool;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -21,6 +23,10 @@ public class StringUtils {
             quote = m.group(1);
             src = (quote == null || quote.trim().length() == 0) ? m.group(2).split("//s+")[0] : m.group(2);
             imageSrcList.add(src);
+        }
+        if (imageSrcList == null || imageSrcList.size() == 0) {
+            Log.e("imageSrcList","资讯中未匹配到图片链接");
+            return null;
         }
         return imageSrcList.toArray(new String[imageSrcList.size()]);
     }
